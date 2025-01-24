0e52e5ba5aac77a471a82e2a46437c0465a99cbb
venkatswan1991 <venkatswan1991@gmail.com>
2025-01-24 07:14:42 +0530

    code added2

--- vars/javaMAVEN.groovy
+++ vars/javaMAVEN.groovy
@@ -1,26 +1,12 @@
 def call(Map configMap){
-
-pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Build Success...'
+    pipeline {
+        agent any
+        stages {
+            stage('Test') { 
+                steps {
+                    echo "Test Stage"
+                }
             }
-        }
-        stage('Test') {
-            steps {
-                echo 'Test Success...'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploy Success...'
-            }
-        }
+        }   
     }
-}
-
-
 }
